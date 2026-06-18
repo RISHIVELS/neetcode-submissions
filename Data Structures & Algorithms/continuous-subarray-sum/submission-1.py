9f05b9e079ea0bca6453a60671eb02cc78d615a1
RISHIVEL S <rishivel10@gmail.com>
2026-06-18 12:01:14 +0530

    Add: continuous-subarray-sum - submission-1

---
+++ Data Structures & Algorithms/continuous-subarray-sum/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        # brute force solution
+
+        for i in range(len(nums)-1):
+            sum = nums[i]
+            for j in range(i+1,len(nums)):
+                sum = sum + nums[j]
+                if sum % k == 0  :
+                    return True 
+
+        return False
