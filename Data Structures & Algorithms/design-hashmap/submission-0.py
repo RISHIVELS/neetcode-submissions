b22aad1624b80d7e75c2fed988bfd4654684655b
RISHIVEL S <rishivel10@gmail.com>
2026-06-19 22:48:55 +0530

    Add: design-hashmap - submission-0

---
+++ Data Structures & Algorithms/design-hashmap/submission-0.py
@@ -0,0 +1,23 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.arr = [-1] * (10**6 + 1)
+
+    def put(self, key: int, value: int) -> None:
+        self.arr[key] = value 
+        return None
+
+    def get(self, key: int) -> int:
+        if self.arr[key] == -1 : 
+            return -1
+        return self.arr[key]
+
+    def remove(self, key: int) -> None:
+        self.arr[key] = -1
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
