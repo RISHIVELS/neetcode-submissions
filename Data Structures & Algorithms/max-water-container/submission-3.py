17d46b44d639496c1ef51f7e8bb6bd0dfc67a672
RISHIVEL S <rishivel10@gmail.com>
2026-06-23 10:00:37 +0530

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.py
@@ -0,0 +1,35 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        # brute force approach time complexity -> O(n^2)
+        # space complexity -> O(1)
+        # _max = 0
+
+        # for i in range(len(heights)):
+        #     for j in range(1,len(heights)):
+        #         distance = j - i
+        #         min_height = min(heights[i],heights[j])
+        #         curr = distance * min_height
+        #         if curr > _max : 
+        #             _max = curr 
+        
+        # return _max
+
+        # optimised solution 
+
+        _max = 0
+        left = 0
+        right = len(heights) - 1
+        while (left < right):
+            distance = right - left 
+            min_height = min(heights[left],heights[right])
+            capacity = distance * min_height
+            if capacity > _max : 
+                _max = capacity 
+            if heights[left] < heights[right]:
+                left += 1
+            else : 
+                right -= 1
+        return _max
+
+
+
